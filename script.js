9c103a641dac0d143f9f2f35f815293b94186af0
RickBy23 <brpalomequeo@gmail.com>
2026-04-24 11:36:54 -0500

    Version 1.0

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const fotos = ["DSCN5374","DSCN5379","DSCN5384","DSCN5386","DSCN5397","DSCN5405","DSCN5411","DSCN5412","DSCN5427","DSCN5428","DSCN5429","DSCN5430","DSCN5432","DSCN5435","DSCN5438","DSCN5443","DSCN5447","DSCN5454","DSCN5460","DSCN5465","DSCN5466","DSCN5474","DSCN5475","DSCN5484","DSCN5487","DSCN5491","DSCN5492","DSCN5494","DSCN5495","DSCN5506","DSCN5514","DSCN5528","DSCN5533","DSCN5536","DSCN5540","DSCN5548","DSCN5550","DSCN5554","DSCN5562"];
-const verticales = ["DSCN5379", "DSCN5411", "DSCN5427", "DSCN5428", "DSCN5429", "DSCN5430", "DSCN5432", "DSCN5435", "DSCN5438", "DSCN5443", "DSCN5454", "DSCN5460", "DSCN5465", "DSCN5466", "DSCN5474", "DSCN5487", "DSCN5492", "DSCN5495", "DSCN5528", "DSCN5536", "DSCN5550", "DSCN5554"];
+const verticales = ["DSCN5379","DSCN5411","DSCN5427","DSCN5428","DSCN5429","DSCN5430","DSCN5432","DSCN5435","DSCN5438","DSCN5443","DSCN5454","DSCN5460","DSCN5465","DSCN5466","DSCN5474","DSCN5487","DSCN5492","DSCN5495","DSCN5528","DSCN5536","DSCN5550","DSCN5554"];
 
 function generarPaginas() {
     const book = $("#book");
     const contraportada = book.children().last();
+
     let i = 0;
     while (i < fotos.length) {
         const page = $('<div class="page"><div class="page-content"></div></div>');
@@ -12,58 +13,69 @@ function generarPaginas() {
         if (fotos[i]) { crearFoto(fotos[i], content, "bottom-right"); i++; }
         page.insertBefore(contraportada);
     }
-    book.turn({ width: 900, height: 600, autoCenter: true, display: 'double', acceleration: true, gradients: true, duration: 1000 });
+
+    book.turn({
+        width: 900,
+        height: 600,
+        autoCenter: true,
+        display: 'double',
+        duration: 1000,
+        when: {
+            turning: function(e, page) {
+                const total = $(this).turn('pages');
+                // Desactivar grosor en portada y contraportada
+                if (page === 1 || page === total) {
+                    $('#album-viewport').removeClass('abierto');
+                } else {
+                    $('#album-viewport').addClass('abierto');
+                }
+            }
+        }
+    });
 }
 
 function crearFoto(nombre, container, posicion) {
     const cont = document.createElement("div");
     cont.className = "foto";
     const esVertical = verticales.includes(nombre);
+    cont.style.width = esVertical ? "190px" : "270px";
+    cont.style.height = esVertical ? "270px" : "190px";
 
-    // Tamaños ligeramente reducidos para que no se salgan del margen
-    cont.style.width = esVertical ? "180px" : "250px";
-    cont.style.height = esVertical ? "250px" : "180px";
-
-    // Margen de seguridad de 50px desde los bordes de la página
     if (posicion === "top-left") {
-        cont.style.left = "50px";
-        cont.style.top = "50px";
+        cont.style.left = "75px";
+        cont.style.top = "75px";
     } else {
-        cont.style.right = "50px";
-        cont.style.bottom = "50px";
+        cont.style.right = "75px";
+        cont.style.bottom = "75px";
     }
 
-    const rot = (Math.random() * 6) - 3; // Rotación sutil
-    cont.style.transform = "rotate(" + rot + "deg)";
-
+    cont.style.transform = `rotate(${(Math.random() * 6) - 3}deg)`;
     const img = new Image();
     img.src = "images_low/" + nombre + ".jpg";
-    cont.onclick = function() { abrirModal(nombre); };
+    cont.onclick = function(e) { e.stopPropagation(); abrirModal(nombre); };
     cont.appendChild(img);
     container.append(cont);
 }
 
 function abrirModal(nombre) {
-    const modal = document.getElementById("modal");
-    const modalImg = document.getElementById("modalImg");
-    const downloadBtn = document.getElementById("downloadBtn");
-
-    modalImg.src = "images/" + nombre + ".jpg";
-    modal.style.display = "flex";
-
-    // Función de descarga directa HD
-    downloadBtn.onclick = function() {
+    document.getElementById("modalImg").src = "images/" + nombre + ".jpg";
+    document.getElementById("modal").style.display = "flex";
+    document.getElementById("downloadBtn").onclick = function() {
         const a = document.createElement("a");
         a.href = "images/" + nombre + ".jpg";
         a.download = nombre + ".jpg";
-        document.body.appendChild(a);
         a.click();
-        document.body.removeChild(a);
     };
 }
 
 function cerrarModal() { document.getElementById("modal").style.display = "none"; }
 function nextPage() { $('#book').turn('next'); }
 function prevPage() { $('#book').turn('previous'); }
+
+document.addEventListener("keydown", function(e) {
+    if (e.key === "Escape") cerrarModal();
+    if (e.key === "ArrowLeft") prevPage();
+    if (e.key === "ArrowRight") nextPage();
+});
 
 $(document).ready(generarPaginas);
